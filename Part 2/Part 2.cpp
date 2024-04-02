cb6a53a11a27f8870e36a7ec54c92989ae89f13e
adilaroisa <adilaroisa688@gmail.com>
2024-04-02 10:58:44 +0700

    menampilkan data dari variabel struct

--- Part 2/Part 2.cpp
+++ Part 2/Part 2.cpp
@@ -28,6 +28,14 @@ int main() {
 
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n \t Kota : " << mhs.alamat.kota;
+	cout << "\n Umur : " << mhs.umur;
 }
 
 
