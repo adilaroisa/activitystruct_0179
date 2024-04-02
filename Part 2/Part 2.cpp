438996908e2da9b6ecc5971dc51cc85f51fbee31
adilaroisa <adilaroisa688@gmail.com>
2024-04-02 11:36:40 +0700

    memperbaiki kesalahan

--- Part 2/Part 2.cpp
+++ Part 2/Part 2.cpp
@@ -9,7 +9,7 @@ struct DetailAlamat {
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
 };
 
@@ -32,7 +32,7 @@ int main() {
 	cout << endl;
 	cout << "\n NIM : " << mhs.nim;
 	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Alamat : ";
 	cout << "\n \t Desa : " << mhs.alamat.desa;
 	cout << "\n \t Kota : " << mhs.alamat.kota;
 	cout << "\n Umur : " << mhs.umur;
