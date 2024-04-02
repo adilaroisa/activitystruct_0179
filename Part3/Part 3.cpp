c6f12f4390354f659c1e7ff4aed03eeba652c3d2
adilaroisa <adilaroisa688@gmail.com>
2024-04-02 12:19:08 +0700

    Membaca input

--- Part3/Part 3.cpp
+++ Part3/Part 3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct DetailAlamat {
@@ -12,9 +13,13 @@ struct Mahasiswa {
 	int umur;
 };
 
-int main()
-{
-    
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
 }
 
 
